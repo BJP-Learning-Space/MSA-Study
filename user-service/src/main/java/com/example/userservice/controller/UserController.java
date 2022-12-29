a2a12896d3502d2a6f8ed781e19402bc0fb5f981
BEOMKING <qjawlsqjacks@naver.com>
2022-12-30 00:44:01 +0900

    spring security add

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -4,9 +4,11 @@ import com.example.userservice.domain.User;
 import com.example.userservice.dto.UserRequest;
 import com.example.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,6 +36,7 @@ public class UserController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody UserRequest userRequest) {
         return userService.createUser(userRequest);
     }

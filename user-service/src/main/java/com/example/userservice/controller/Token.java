5ea6bdf5b32996151a5499a0d8c60a8ec7949c6c
BEOMKING <qjawlsqjacks@naver.com>
2023-01-14 21:10:39 +0900

    Refresh Test

--- user-service/src/main/java/com/example/userservice/controller/Token.java
+++ user-service/src/main/java/com/example/userservice/controller/Token.java
@@ -3,11 +3,9 @@ package com.example.userservice.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Getter
 @Setter
-@RefreshScope
 @ConfigurationProperties("token")
 public class Token {
     private String secret;

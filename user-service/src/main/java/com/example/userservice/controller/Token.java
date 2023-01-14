f46cc1c506bb4b9d01fe071220cdd6006528668f
BEOMKING <qjawlsqjacks@naver.com>
2023-01-14 11:26:01 +0900

    Refresh Test

--- user-service/src/main/java/com/example/userservice/controller/Token.java
+++ user-service/src/main/java/com/example/userservice/controller/Token.java
@@ -3,9 +3,12 @@ package com.example.userservice.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties("token")
 public class Token {
     private String secret;
